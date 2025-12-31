d06bec828d5004b7c4da4f6cf437b22b8c1463a3
Divyakumaresan <101942248+DivyaKumaresa@users.noreply.github.com>
2025-12-31 12:44:46 +0530

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,11 @@
+# Directories to back up (space separated)
+SOURCE_DIRS="/opt/appdata"
+
+# Backup destination directory
+BACKUP_DIR="/opt/backups"
+
+# Log file location
+LOG_FILE="/var/log/backup/backup.log"
+
+# Retention policy (in days)
+RETENTION_DAYS=7
